2b0fd6896880e836c9002584b9db0b8e4e9d24ce
EderAmorimIFMS <eder.amorim@estudante.ifms.edu.br>
2023-09-19 02:01:18 -0400

    commit: correções e adicionando codigo

--- src/app/pages/alterar/page.js
+++ src/app/pages/alterar/page.js
@@ -6,48 +6,80 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function Alterar() {
+  const [busca_nome, setBusca_nome] = ''
   const [usuario, setUsuario] = useState({
     nome: '',
     email: '',
     password: '',
   });
+
   const { push, refresh } = useRouter();
 
-  const handlerAlterar = async (e) => {
+const btnBuscar = async (e) => {
+    e.preventDefault();
+    try {
+        
+
+    } catch {
+        toast.error("Erro na busca!");
+    }
+  }
+
+  const btnAlterar = async (e) => {
     e.preventDefault();
     try {
       const userAuth = await handlerAcessUser(user);
       if(userAuth.token == undefined){
         toast.error("Erro no email ou senha!");
       }
-      push('/pages/dashboard');
 
     } catch {
         toast.error("Erro na aplicação");
     }
   }
+  
   return (
     <div>
       <div class="container">
         <h1>Alterar</h1>
         <div class="form">
-            <form>
-                <input type="text" name="nome" id="nome" placeholder="ESCREVA O NOME DE SUA PESSOA" class="input_text">
+            <form onSubmit={btnBuscar}>
+                <input
+                type="text"
+                name="busca_nome"
+                placeholder="ESCREVA O NOME DO USUARIO QUE DESEJA ALTERAR" 
+                class="input_text"
+                onChange={(e) => { setBuscanome(e.target.value)}}>
+                
+                <div class="botoes">
+                     <button>BUSCAR</button>
+                </div>
             </form>
-            <div class="botoes">
-                <button>BUSCAR</button>
-            </div>
         </div>
 
         <div>
-             <form>
-                <input type="text" name="nome" placeholder="ESCREVA O NOME DO USUARIO A SE ALTERAR" class="input_text">
-                <input type="text" name="email" id="nome" placeholder="ESCREVA O EMAIL DO USUARIO A SE ALTERAR" class="input_text">
-                <input type="password" name="password" placeholder="ESCREVA A SENHA DO USUARIO A SE ALTERAR" class="input_text">
+             <form onSubmit={btnAlterar}>
+                <input 
+                type="text" 
+                placeholder="ESCREVA O NOME DO USUARIO A SE ALTERAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, nome: e.target.value }) }}>
+                <input 
+                type="email" 
+                placeholder="ESCREVA O EMAIL DO USUARIO A SE ALTERAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, email: e.target.value }) }}>
+                <input 
+                type="password" 
+                placeholder="ESCREVA A SENHA DO USUARIO A SE ALTERAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, password: e.target.value }) }}>
+                
+                <div class="botoes">
+                     <button>ALTERAR</button>
+                </div>
             </form>
-            <div class="botoes">
-                <button>ALTERAR</button>
-            </div>
+            
         </div>
 
       <ToastContainer />
--- src/app/pages/registrar/page.js
+++ src/app/pages/registrar/page.js
@@ -7,40 +7,51 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function Registrar() {
+  const [busca_nome, setBusca_nome] = ''
   const [usuario, setUsuario] = useState({
     nome: '',
     email: '',
     password: '',
   });
-  const { push, refresh } = useRouter();
 
-  const handlerRegistrar = async (e) => {
+  const btnRegistrar = async (e) => {
     e.preventDefault();
     try {
-      const userAuth = await handlerAcessUser(user);
-      if(userAuth.token == undefined){
-        toast.error("Erro no email ou senha!");
-      }
-      push('/pages/dashboard');
+      
 
     } catch {
         toast.error("Erro na aplicação");
     }
   }
+  
   return (
     <div>
       <div class="container">
-        <h1>Registrar</h1>
+        <h1>Alterar</h1>
 
         <div>
-             <form>
-                <input type="text" name="nome" placeholder="ESCREVA O NOME DO USUARIO A SE REGISTRAR" class="input_text">
-                <input type="text" name="email" placeholder="ESCREVA O EMAIL DO USUARIO A SE REGISTRAR" class="input_text">
-                <input type="password" name="password" placeholder="ESCREVA A SENHA DO USUARIO A SE REGISTRAR" class="input_text">
+             <form onSubmit={btnRegistrar}>
+                <input 
+                type="text" 
+                placeholder="ESCREVA O NOME DO USUARIO A SE REGISTRAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, nome: e.target.value }) }}>
+                <input 
+                type="email" 
+                placeholder="ESCREVA O EMAIL DO USUARIO A SE REGISTRAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, email: e.target.value }) }}>
+                <input 
+                type="password" 
+                placeholder="ESCREVA A SENHA DO USUARIO A SE REGISTRAR" 
+                class="input_text"
+                onChange={(e) => { setUsuario({ ...usuario, password: e.target.value }) }}>
+                
+                <div class="botoes">
+                     <button>ALTERAR</button>
+                </div>
             </form>
-            <div class="botoes">
-                <button>REGISTRAR</button>
-            </div>
+            
         </div>
 
       <ToastContainer />
